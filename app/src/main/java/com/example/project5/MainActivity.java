2f978831becc36ce268eb80aa0cc8670b7a50494
yashpat1602 <ysp21@scarletmail.rutgers.edu>
2022-12-05 23:26:54 -0500

    yash

--- app/src/main/java/com/example/project5/MainActivity.java
+++ app/src/main/java/com/example/project5/MainActivity.java
@@ -19,10 +19,10 @@ public class MainActivity extends AppCompatActivity {
 
     //private ActivityMainBinding binding;
     BottomNavigationView navView;
-    ChicagoStyle chicagoStyle = new ChicagoStyle();
-    NewYorkStyle newYorkStyle = new NewYorkStyle();
-    CurrentOrder currentOrder = new CurrentOrder();
-    StoreOrders storeOrders = new StoreOrders();
+    ChicagoStyle chicagoStyleFragment = new ChicagoStyle();
+    NewYorkStyle newYorkStyleFragment = new NewYorkStyle();
+    CurrentOrder currentOrderFragment = new CurrentOrder();
+    StoreOrders storeOrdersFragment = new StoreOrders();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,37 +31,29 @@ public class MainActivity extends AppCompatActivity {
         //binding = ActivityMainBinding.inflate(getLayoutInflater());
         //setContentView(binding.getRoot());
 
-        navView = (BottomNavigationView)findViewById(R.id.nav_view);
+        navView = findViewById(R.id.nav_view);
 
-        getSupportFragmentManager().beginTransaction().replace(R.id.container, chicagoStyle);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, chicagoStyleFragment).commit();
         navView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(MenuItem item) {
                 switch(item.getItemId()) {
-                    case R.id.chicagoStyle:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container, chicagoStyle).commit();
+                    case R.id.navigation_chicago:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, chicagoStyleFragment).commit();
                         return true;
-                    case R.id.newYorkStyle:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container, newYorkStyle).commit();
+                    case R.id.navigation_newyork:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, newYorkStyleFragment).commit();
                         return true;
-                    case R.id.currentOrder:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container, currentOrder).commit();
+                    case R.id.navigation_current:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, currentOrderFragment).commit();
                         return true;
-                    case R.id.storeOrders:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container, storeOrders).commit();
+                    case R.id.navigation_store:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, storeOrdersFragment).commit();
                         return true;
                 }
                 return false;
             }
         });
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-//        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-//                R.id.navigation_chicago, R.id.navigation_newyork, R.id.navigation_current, R.id.navigation_store)
-//                .build();
-//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-//        NavigationUI.setupWithNavController(binding.navView, navController);
 
 
     }
