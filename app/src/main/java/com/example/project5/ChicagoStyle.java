c2e9981fbf2864cb83d32682a92acde24317d4d0
SiddhP2003 <smp436@scarletmail.rutgers.edu>
2022-12-06 19:36:05 -0500

    Added Methods

--- app/src/main/java/com/example/project5/ChicagoStyle.java
+++ app/src/main/java/com/example/project5/ChicagoStyle.java
@@ -7,13 +7,44 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.text.DecimalFormat;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link ChicagoStyle#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ChicagoStyle extends Fragment {
+public class ChicagoStyle extends Fragment implements AdapterView.OnItemSelectedListener, AdapterView.OnItemClickListener {
+     Spinner flavorSpinner;
+    private Spinner sizeSpinner;
+    private TextView crustTextView;
+    private ImageView pizzaFlavorImageView;
+    private ListView toppings;
+    private EditText priceEditText;
+    private String[] flavors = {"Deluxe", "BBQ Chicken",
+            "Meatzza", "Build Your Own"};
+    private String[] sizes = {"Large", "Medium", "Small"};
+    private String[] toppingList = {Topping.BEEF.topping(),
+                        Topping.CHEDDAR.topping(),Topping.PROVOLONE.topping(),Topping.HAM.topping(),
+                                Topping.PEPPERONI.topping(), Topping.MUSHROOM.topping(), Topping.BBQChicken.topping()
+                                , Topping.ONION.topping(),Topping.SAUSAGE.topping(),
+                                Topping.GREENPEPPER.topping(), Topping.BLACKOLIVES.topping(),
+                                Topping.PINEAPPLE.topping(), Topping.JALAPENOPEPPERS.topping()};
+    private ArrayAdapter<String> flavorAdapter;
+    private ArrayAdapter<String> sizeAdapter;
+    private ArrayAdapter<String> toppingAdapter;
+    private PizzaFactory pizzaFactory = new ChicagoPizza();
+    private Pizza pizza;
 
 //    // TODO: Rename parameter arguments, choose names that match
 //    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -48,17 +79,120 @@ public class ChicagoStyle extends Fragment {
 //
 //    @Override
 //    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        if (getArguments() != null) {
-//            mParam1 = getArguments().getString(ARG_PARAM1);
-//            mParam2 = getArguments().getString(ARG_PARAM2);
-//        }
+//
+////        if (getArguments() != null) {
+////            mParam1 = getArguments().getString(ARG_PARAM1);
+////           mParam2 = getArguments().getString(ARG_PARAM2);
+////        }
+//
+//
 //    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_chicago_style, container, false);
+        flavorSpinner = view.findViewById(R.id.flavorSpinner);
+        sizeSpinner = view.findViewById(R.id.sizeSpinner);
+        flavorAdapter = new ArrayAdapter<String>(this.getActivity(), androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, flavors);
+        sizeAdapter = new ArrayAdapter<String>(this.getActivity(), androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, sizes);
+        flavorSpinner.setAdapter(flavorAdapter);
+        sizeSpinner.setAdapter(sizeAdapter);
+        crustTextView = view.findViewById(R.id.crustTextView);
+        pizzaFlavorImageView = view.findViewById(R.id.pizzaFlavorImageView);
+        //priceEditText = view.findViewById(R.id.)
+        //toppings = view.findViewById(R.id);
+        toppings.setOnItemClickListener(this);
+        toppingAdapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_list_item_1,toppingList);
+        toppings.setAdapter(toppingAdapter);
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chicago_style, container, false);
+        return view;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        switch(adapterView.getId()){
+            case R.id.flavorSpinner:
+                String selectedFlavor = flavorSpinner.getSelectedItem().toString();
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+    private void setPizza(String flavor){
+        if(flavor.equals("Build Your Own")){
+            pizza = pizzaFactory.createBuildYourOwn();
+        }
+        else if(flavor.equals("BBQ Chicken")){
+            pizza = pizzaFactory.createBBQChicken();
+        }
+        else if(flavor.equals("Deluxe")){
+            pizza = pizzaFactory.createDeluxe();
+        }
+        else if(flavor.equals("Meatzza")){
+            pizza = pizzaFactory.createMeatzza();
+        }
+    }
+
+    private void currentCrust(String flavor){
+        if(flavor.equals("Build Your Own") ||flavor.equals("BBQ Chicken")){
+            crustTextView.setText(Crust.PAN.crust());
+        }
+        else if(flavor.equals("Deluxe")){
+            crustTextView.setText(Crust.DEEPDISH.crust());
+        }
+        else if(flavor.equals("Meatzza")){
+            crustTextView.setText(Crust.STUFFED.crust());
+        }
+    }
+
+    private void setImage(String flavor) throws FileNotFoundException {
+        if(flavor.equalsIgnoreCase("Build Your Own")){
+            pizzaFlavorImageView.setImageResource(R.drawable.);
+
+        }
+        else if(flavor.equalsIgnoreCase("Meatzza")){
+            pizzaFlavorImageView.setImageResource(R.drawable.);
+        }
+        else if(flavor.equalsIgnoreCase("BBQ Chicken")){
+            pizzaFlavorImageView.setImageResource(R.drawable.);
+        }
+        else{
+            //deluxe image
+            pizzaFlavorImageView.setImageResource(R.drawable.);
+        }
+        //pizzaFlavorImageView.setImage(image);
+    }
+
+    private void changePrice(){
+        DecimalFormat format = new DecimalFormat("#.##");
+        priceEditText.setText(format.format(pizza.price()));
+    }
+
+    private void setToppings(String flavor){
+        if(flavor.equals("Build Your Own")){
+            for(int i = 0; i < toppingAdapter.getCount(); i++){
+                if(toppingAdapter.isEnabled())
+            }
+        }
+        else if(flavor.equals("BBQ Chicken")){
+
+        }
+        else if(flavor.equals("Deluxe")){
+        }
+        else if(flavor.equals("Meatzza")){
+
+        }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
     }
 }
